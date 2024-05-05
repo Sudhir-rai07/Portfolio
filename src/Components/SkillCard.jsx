b3adf10b783ac9d123352c05ad44d8d65fa7042f
Sudhir Rai <128808869+Sudhir-rai07@users.noreply.github.com>
2024-05-05 22:17:15 +0530

    Update SkillCard.jsx

--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -3,8 +3,8 @@ import { motion } from 'framer-motion'
 
 const SkillCard = ({url, name}) => {
   return ( 
-    <motion.div 
-        <div className='px-1 py-1 cursor-pointer h-20 w-20 mr-4 mb-4 border-2 border-gray-300 rounded-lg bg-white flex flex-col  items-center transition-all duration-200 hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
+    <motion.div>
+      <div className='px-1 py-1 cursor-pointer h-20 w-20 mr-4 mb-4 border-2 border-gray-300 rounded-lg bg-white flex flex-col  items-center transition-all duration-200 hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
             <img src={url} alt="" className='w-full h-full rounded-lg'/>
             <span className='absolute left-0 hidden font-semibold -top-5 group-hover:block'>{name}</span>
         </div>
