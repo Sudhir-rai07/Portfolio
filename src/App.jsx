a2dbe67be076ad293af03da8f3d5ed448ecb7923
Sudhir Rai <sudhirrai128@gmail.com>
2024-06-13 17:39:52 +0530

    Add resume

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ const App = () => {
                 className="px-5 py-2.5 relative rounded group overflow-hidden font-medium bg-red-50 text-red-600 inline-block"
               >
                 <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-red-600 group-hover:h-full opacity-90"></span>
-                <span className="relative group-hover:text-white">Download cv</span>
+                <a href="/RESUME.pdf" download={"resume.pdf"} className="relative group-hover:text-white">Download cv</a>
               </a>
             </button>
           </div>
