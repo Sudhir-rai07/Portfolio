9f083d014c782a5ac334bcafb20b2c8007a69b06
Sudhir Rai <sudhirrai128@gmail.com>
2024-12-01 15:55:26 +0530

    Update Resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import spider from "./image/spider.png";
 import { motion } from "framer-motion";
 import Skills from "./Components/Skills";
@@ -71,7 +71,7 @@ const App = () => {
                 className="px-5 py-2.5 relative rounded group overflow-hidden font-medium bg-red-50 text-red-600 inline-block"
               >
                 <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-red-600 group-hover:h-full opacity-90"></span>
-                <a href="/RESUME00.pdf" download={"resume.pdf"} className="relative group-hover:text-white">Download cv</a>
+                <a href="/Sudhir_Resume.pdf" download={"resume.pdf"} className="relative group-hover:text-white">Download cv</a>
               </a>
             </button>
           </div>
